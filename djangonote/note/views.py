185ba224b55af84b18b8a6900e99635d5b4551ca
Aman-Yerpude <yerpudeaman@gmail.com>
2019-09-22 22:11:24 +0530

    registration module

--- djangonote/note/views.py
+++ djangonote/note/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.filter(author=request.user)
     }
     return render(request, 'note/home.html', context)
 
