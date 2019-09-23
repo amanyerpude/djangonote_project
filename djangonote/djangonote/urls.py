3b2e9b7e2c8f7d001cf60fdac41ce9ea2fc50804
Aman-Yerpude <yerpudeaman@gmail.com>
2019-09-23 15:31:49 +0530

    post-create-update-delete

--- djangonote/djangonote/urls.py
+++ djangonote/djangonote/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('note/', include('note.urls')),
-    # path('profile/', user_views.profile, name='profile'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- djangonote/note/urls.py
+++ djangonote/note/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.home, name='note-home'),
+    path('', login_required(PostListView.as_view()), name='note-home'), #path('', views.home, name='note-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='note-about'),
 ]
+
